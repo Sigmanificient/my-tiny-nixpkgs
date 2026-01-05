252af5661dbe3f1625fd279dccbf5b8a27d02252
Sigmanificient <edhyjox@gmail.com>
2026-01-05 02:30:04 +0100

    Setup a flake that expose MY_TINY_NIXPKGS_SOURCE

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "my tiny nixpkgs";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    applySystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    eachSystem = f:
+      applySystems (system:
+        f {
+          inherit system;
+          pkgs = nixpkgs.legacyPackages.${system};
+        });
+  in {
+    formatter = eachSystem ({pkgs, ...}: pkgs.alejandra);
+
+    devShells = eachSystem ({
+      pkgs,
+      ...
+    }: {
+      default = pkgs.mkShell {
+        env.MY_TINY_NIXPKGS_SOURCE = nixpkgs.outPath;
+      };
+    });
+  };
+}
